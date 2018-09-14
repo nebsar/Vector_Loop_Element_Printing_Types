5be06dfd9167b70a647f4ff230491d76a702a7c7
Eagleye <Eagleye@EagleEye>
2018-09-13 23:12:03 -0400

    Vector_Loop_Element_Printing_Types

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: Eagleye
+ *
+ * Created on September 13, 2018, 10:58 PM
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int printElement(int element) {
+    std::cout << element << " ";
+}
+
+int main(int argc, char** argv) {
+
+    std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    std::cout << "from normal loop :  ";
+    for (int i = 0; i < v.size(); ++i)
+        std::cout << v[i] << " ";
+
+    std::cout << '\n';
+    std::cout << "from elem : v loop :  ";
+
+    for (auto elem : v)
+        std::cout << elem << " ";
+
+    std::cout << '\n';
+    std::cout << "from for_each lambda loop :  ";
+
+    for_each(v.begin(), v.end(), [&](int elem) {
+        std::cout << elem << " ";;
+    });
+
+    std::cout << '\n';
+    std::cout << "from for_each function loop :  ";
+
+    for_each(v.begin(), v.end(), printElement);
+
+
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=vector_loop_element_printing_types
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/vector_loop_element_printing_types
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=vectorloopelementprintingtypes.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/vectorloopelementprintingtypes.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=vector_loop_element_printing_types
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/vector_loop_element_printing_types
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=vectorloopelementprintingtypes.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/vectorloopelementprintingtypes.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
